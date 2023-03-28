d28868f2589c20b64e9b1ceffda499df2b04edde
Chad Wright <justdeejays@gmail.com>
2023-03-28 13:42:29 +0200

    fix: add full screen form popup for mobile

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -1,5 +1,7 @@
-import { useState, ReactNode } from "react";
+import { useState } from "react";
 import Button from "./button";
+
+import classNames from "classnames";
 
 interface Props {
   cssClasses?: string;
@@ -11,10 +13,14 @@ const ContactForm = ({ cssClasses }: Props) => {
   const [showSerenePark, setShowSerenePark] = useState(false);
 
   return (
-    <>
+    <div className={`${cssClasses}`}>
       {/* the crescent form */}
 
-      <div className={`mt-10 max-w-[1400px] mx-auto ${cssClasses}`}>
+      <div
+        className={classNames("mt-10 max-w-[1400px] mx-auto", {
+          "-mx-6": showCrescent,
+        })}
+      >
         {!showCrescent && !showEastlands && !showSerenePark && (
           <>
             <p className="text-white mb-10">
@@ -32,8 +38,8 @@ const ContactForm = ({ cssClasses }: Props) => {
         )}
 
         {showCrescent && (
-          <div className="flex flex-col gap-6">
-            <h4 className="text-white text-center font-thin tablet:text-left">
+          <div className="flex flex-col gap-6 fixed top-0 h-screen w-full bg-blue z-20 pt-10 px-6">
+            <h4 className="text-white text-center font-light tablet:text-left">
               The Crescent Lodge
             </h4>
             <form action="" method="POST" className="flex flex-col gap-7">
@@ -82,10 +88,10 @@ const ContactForm = ({ cssClasses }: Props) => {
                   required
                   placeholder="Type your message here"
                   className="pl-2 py-1.5"
-                  rows={4}
+                  rows={3}
                 />
               </div>
-              <div className="flex justify-between mt-4">
+              <div className="flex justify-between mt-2">
                 <Button form onClick={() => setShowCrescent(false)}>
                   Submit
                 </Button>
@@ -100,7 +106,11 @@ const ContactForm = ({ cssClasses }: Props) => {
 
       {/* eastlands form form */}
 
-      <div className={`mt-10 max-w-[1400px] mx-auto ${cssClasses}`}>
+      <div
+        className={classNames("mt-10 max-w-[1400px] mx-auto", {
+          "-mx-6": showEastlands,
+        })}
+      >
         {!showCrescent && !showEastlands && !showSerenePark && (
           <Button
             mobileHomesForm
@@ -112,8 +122,8 @@ const ContactForm = ({ cssClasses }: Props) => {
         )}
 
         {showEastlands && (
-          <div className="flex flex-col gap-6">
-            <h4 className="text-white text-center font-thin tablet:text-left">
+          <div className="flex flex-col gap-6 fixed top-0 h-screen w-full bg-blue z-20 pt-10 px-6">
+            <h4 className="text-white text-center font-light tablet:text-left">
               Eastlands Mature Lifestyle Estate
             </h4>
             <form action="" method="POST" className="flex flex-col gap-7">
@@ -162,10 +172,10 @@ const ContactForm = ({ cssClasses }: Props) => {
                   required
                   placeholder="Type your message here"
                   className="pl-2 py-1.5"
-                  rows={4}
+                  rows={3}
                 />
               </div>
-              <div className="flex justify-between mt-4">
+              <div className="flex justify-between mt-2">
                 <Button form onClick={() => setShowEastlands(false)}>
                   Submit
                 </Button>
@@ -180,7 +190,11 @@ const ContactForm = ({ cssClasses }: Props) => {
 
       {/* serenepark form form */}
 
-      <div className={`mt-10 max-w-[1400px] mx-auto ${cssClasses}`}>
+      <div
+        className={classNames("mt-10 max-w-[1400px] mx-auto", {
+          "-mx-6": showSerenePark,
+        })}
+      >
         {!showCrescent && !showEastlands && !showSerenePark && (
           <Button
             mobileHomesForm
@@ -192,8 +206,8 @@ const ContactForm = ({ cssClasses }: Props) => {
         )}
 
         {showSerenePark && (
-          <div className="flex flex-col gap-6">
-            <h4 className="text-white text-center font-thin tablet:text-left">
+          <div className="flex flex-col gap-6 fixed top-0 h-screen w-full bg-blue z-20 pt-10 px-6">
+            <h4 className="text-white text-center font-light tablet:text-left">
               Serenepark Retirement Centre
             </h4>
             <form action="" method="POST" className="flex flex-col gap-7">
@@ -242,10 +256,10 @@ const ContactForm = ({ cssClasses }: Props) => {
                   required
                   placeholder="Type your message here"
                   className="pl-2 py-1.5"
-                  rows={4}
+                  rows={3}
                 />
               </div>
-              <div className="flex justify-between mt-4">
+              <div className="flex justify-between mt-2">
                 <Button form onClick={() => setShowSerenePark(false)}>
                   Submit
                 </Button>
@@ -257,7 +271,7 @@ const ContactForm = ({ cssClasses }: Props) => {
           </div>
         )}
       </div>
-    </>
+    </div>
   );
 };
 
