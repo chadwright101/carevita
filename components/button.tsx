fe3b2f045b1c1a3e40558374dc00d89c5b69f0e1
Chad Wright <justdeejays@gmail.com>
2023-04-03 15:17:13 +0200

    fix: adjust button styling

--- components/button.tsx
+++ components/button.tsx
@@ -118,7 +118,7 @@ const Button = ({
   } else {
     return (
       <button
-        className={`px-12 py-2.5 text-subheading rounded-[22px] bg-blue text-white drop-shadow-md border-lightGreen border-[2px] tablet:hover:bg-green tablet:px-10 tablet:py-1.5 tablet:rounded-[20px] ${cssClasses}`}
+        className={`px-12 py-2.5 text-subheading bg-blue text-white drop-shadow-md tablet:hover:bg-green tablet:px-10 tablet:py-1.5 ${cssClasses}`}
       >
         <Link href={url!}>{children || "View More"}</Link>
       </button>
