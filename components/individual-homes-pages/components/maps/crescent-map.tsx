60a75a8d2fe1791fe1e5a7c47af6f75dff279631
Chad Wright <justdeejays@gmail.com>
2023-04-05 20:00:20 +0200

    feat: add styled map loading... div

--- components/individual-homes-pages/components/maps/crescent-map.tsx
+++ components/individual-homes-pages/components/maps/crescent-map.tsx
@@ -8,13 +8,12 @@ const CrescentMap = ({ cssClasses }: Props) => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
   });
-  if (!isLoaded) return <div>Loading...</div>;
-  return <Map cssClasses={cssClasses} />;
-};
-
-export default CrescentMap;
-
-const Map = ({ cssClasses }: Props) => {
+  if (!isLoaded)
+    return (
+      <div className="mt-10 bg-lightGreen/20 text-heading font-thin text-center py-16 max-w-[1400px]">
+        Map loading...
+      </div>
+    );
   return (
     <GoogleMap
       zoom={13.75}
@@ -25,3 +24,5 @@ const Map = ({ cssClasses }: Props) => {
     </GoogleMap>
   );
 };
+
+export default CrescentMap;
