21d6d8cf9c16625fd1599d8448369f49011c363d
Chad Wright <justdeejays@gmail.com>
2023-05-15 11:26:38 +0200

    fix: remove our schools info & add iphone autoplay for home page hero

--- components/pages/home/hero.tsx
+++ components/pages/home/hero.tsx
@@ -9,7 +9,9 @@ const Hero = ({ cssClasses }: Props) => {
         autoPlay
         muted
         loop
-        className="w-full h-screen tabletLarge:h-[600px] object-cover desktop:max-w-[1400px] mx-auto"
+        className="w-full h-[450px] tabletLarge:h-[600px] object-cover desktop:max-w-[1400px] mx-auto"
+        poster="https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/carevita/the-crescent/sliders/hero/DJI_0961.jpg"
+        playsInline
       >
         <source src="https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/carevita/home-page/the-crescent-test.mp4" />
       </video>
