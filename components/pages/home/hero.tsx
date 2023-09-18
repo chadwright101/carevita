5dc1fbaf5219c5a86de181c9949f2b7939dd5388
Chad Wright <justdeejays@gmail.com>
2023-09-18 12:07:15 +0200

    feat: add final videos to hero with window width listener

--- components/pages/home/hero.tsx
+++ components/pages/home/hero.tsx
@@ -1,20 +1,41 @@
+import { useContext } from "react";
+
+import WindowWidthContext from "@/components/utils/window-width-context";
+
 interface Props {
   cssClasses?: string;
 }
 
 const Hero = ({ cssClasses }: Props) => {
+  const { windowWidth } = useContext(WindowWidthContext);
   return (
     <section className={`${cssClasses}`}>
-      <video
-        autoPlay
-        muted
-        loop
-        className="w-full h-[450px] tabletLarge:h-[600px] object-cover desktop:max-w-[1400px] mx-auto"
-        poster="https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/carevita/video-poster-carevita.webp"
-        playsInline
-      >
-        <source src="https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/carevita/the-crescent-test.mp4" />
-      </video>
+      {windowWidth <= 650 && (
+        <video
+          autoPlay
+          muted
+          loop
+          className="w-full h-[450px] tabletLarge:h-[600px] object-cover desktop:max-w-[1400px] mx-auto"
+          poster="https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/carevita/video-poster-carevita.webp"
+          playsInline
+        >
+          <source src="https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/carevita/carevita-mobile-video.webm" />
+          <source src="https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/carevita/carevita-mobile-video.mp4" />
+        </video>
+      )}
+      {windowWidth > 650 && (
+        <video
+          autoPlay
+          muted
+          loop
+          className="w-full h-[450px] tabletLarge:h-[600px] object-cover desktop:max-w-[1400px] mx-auto"
+          poster="https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/carevita/video-poster-carevita.webp"
+          playsInline
+        >
+          <source src="https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/carevita/carevita-desktop-video.webm" />
+          <source src="https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/carevita/carevita-desktop-video.mp4" />
+        </video>
+      )}
     </section>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import Footer from "@/components/navigation/footer";
 import Header from "@/components/navigation/header";
+import WindowWidthListener from "@/components/utils/window-width-listeners";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+      <WindowWidthListener>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </WindowWidthListener>
     </>
   );
 }
