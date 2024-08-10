929ad485db06385fd7eefe19dba9306e6fa62a60
Chad Wright <justdeejays@gmail.com>
2024-08-10 17:40:39 +0200

    feat: revert back to aws links

--- next.config.js
+++ next.config.js
@@ -8,20 +8,6 @@ module.exports = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1400],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname:
-          "the-wright-designs-website-images.s3.af-south-1.amazonaws.com",
-        port: "",
-        pathname: "/carevita/**",
-      },
-      {
-        protocol: "https",
-        hostname:
-          "the-wright-designs-website-images.s3.af-south-1.amazonaws.com",
-        port: "",
-        pathname: "/carevita-fit/**",
-      },
-      {
         protocol: "http",
         hostname: "wordpress.carevita.co.za",
         port: "",
